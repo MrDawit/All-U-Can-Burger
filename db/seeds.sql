087c8d725ffb8fbd6bf1bd7d27a099379e5575bc
shalom dawit <shalom.dawit@gmail.com>
2021-01-18 13:22:21 -0500

    jawsdb fix

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 USE burgers_db;
 
 INSERT burgers (burger_name,devoured)
-VALUES ("Spicy Chedar",true),("Veggie's Nightmare", true),("Mushroom Heaven", true);
+VALUES ("Spicy Chedar",false),("Veggie's Nightmare", true),("Mushroom Heaven", true);
