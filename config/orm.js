205319cf23011fdac77bf58888ef003beb672879
shalom dawit <shalom.dawit@gmail.com>
2021-01-18 21:39:46 -0500

    heroku db syntax query fix

--- config/orm.js
+++ config/orm.js
@@ -15,18 +15,21 @@ class Orm {
 
   selectAll(cb) {
     
-    connection.query('SELECT * FROM burgers_db.burgers', function (err, res) {
+    // connection.query('SELECT * FROM burgers_db.burgers', function (err, res) {
+      connection.query('SELECT * FROM burgers', function (err, res) {
       if (err) throw err;
       cb(res);
     });
   };
 
   insertOne() {
-    return 'INSERT INTO burgers_db.burgers (burger_name) VALUE ';
+    // return 'INSERT INTO burgers_db.burgers (burger_name) VALUE ';
+    return 'INSERT INTO burgers (burger_name) VALUE ';
   };
 
   updateOne(col, value, cb) {
-    connection.query("UPDATE burgers_db.burgers SET ? WHERE ? ", [col, value], (err, res) => {
+    // connection.query("UPDATE burgers_db.burgers SET ? WHERE ? ", [col, value], (err, res) => {
+      connection.query("UPDATE burgers SET ? WHERE ? ", [col, value], (err, res) => {
       if (err) throw err;
       cb(res);
     });
@@ -35,7 +38,8 @@ class Orm {
 
   removeOne(value, cb) {
     //example: DELETE FROM `burgers_db`.`burgers` WHERE (`id` = '14');
-    connection.query("DELETE FROM burgers_db.burgers WHERE ? ", [value], (err, res) => {
+    // connection.query("DELETE FROM burgers_db.burgers WHERE ? ", [value], (err, res) => {
+      connection.query("DELETE FROM burgers WHERE ? ", [value], (err, res) => {
       if (err) throw err;
       cb(res);
     });
